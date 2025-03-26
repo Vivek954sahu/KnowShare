519b11d81be19212d3679e041aca268f50695996
Vivek954sahu <vivek954sahu@gmail.com>
2025-03-26 13:14:04 +0530

    Comments error resolve

--- client/public/assets/javascripts/comments.js
+++ client/public/assets/javascripts/comments.js
@@ -12,7 +12,7 @@ export async function loadComments(postId) {
 
     commentsContainer.innerHTML = comments.map(comment => `
       <div class="comment">
-        <p><strong>${comment.username || 'Anonymous'}</strong>:</p>
+        <p><strong>${comment.username?.username || 'Anonymous'}</strong>:</p>
         <p>${comment.content}</p>
         <small>${new Date(comment.createdAt).toLocaleString()}</small>
       </div>
